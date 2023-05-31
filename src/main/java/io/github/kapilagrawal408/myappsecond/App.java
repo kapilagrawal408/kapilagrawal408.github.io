4ac45d3f89185cb9cec215d2dc20c24abf94eb22
kapil agrawal <kapilagrawal.408@gmail.com>
2023-05-31 21:56:02 +0530

    Initial commit

---
+++ src/main/java/io/github/kapilagrawal408/myappsecond/App.java
@@ -0,0 +1,27 @@
+package io.github.kapilagrawal408.myappsecond;
+
+/**
+ * This is a simple class that has method related to mathematical functions
+ * @author Masai
+ */
+public class App {
+	/**
+	 * method to add two numbers and return their addition
+	 * @param a The first value (the LHS operand for +)
+	 * @param b The second value (the RHS operand for +)
+	 * @return addition of the value of parameters a and b
+	 */
+	public static int add(int a, int b) {
+		return (a + b);
+	}
+	
+	/**
+	 * method to subtract two numbers and return their subtraction
+	 * @param a The first value (the LHS operand for -)
+	 * @param b The second value (the RHS operand for -)
+	 * @return subtraction of the value of parameters a and b
+	 */
+	public static int sub(int a, int b) {
+		return (a - b);
+	}
+}
---
+++ src/test/java/io/github/kapilagrawal408/myappsecond/AppTest.java
@@ -0,0 +1,20 @@
+package io.github.kapilagrawal408.myappsecond;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
